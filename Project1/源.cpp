452860c462938421bbd8e8a5a1b398b9698bf85f
郎明彦 <2898703704@qq.com>
2022-04-17 04:26:45 +0800

    学习

---
+++ Project1/源.cpp
@@ -0,0 +1,683 @@
+# define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+
+//#include <stdio.h>
+//
+//#include <stdio.h>
+//
+//int main() 
+//{
+//	printf("郎明彦是个帅哥\n");
+//	printf("%d\n", 50);
+//
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int age = 2022;
+//	double weight = 65.62;
+//
+//	age = age + 1;
+//	weight = weight - 10;
+//	printf("%d\n",age);
+//	printf("%lf\n", weight);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 19;
+//
+//	return 2;
+//}
+
+
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int sum = 0;
+//	scanf("%d %d", &a, &b);
+//	sum = a + b;
+//	printf("sum = %d\n", sum);
+//
+//	return 3;
+//}
+
+//int main()
+//{
+//
+//	int a[10] = {1};
+//	int b = 0;
+//	for (b = 0; b < 10; b++)
+//	{
+//		scanf("%d", &a[b]);
+//	}
+//	int 最大值 = a[0];
+//	for (b = 1; b < 10; b++)
+//	{
+//		if (a[b] > 最大值)
+//		{
+//			最大值 = a[b];
+//		}
+//	}
+//	printf("最大值 =%d\n", 最大值);
+//
+//	return 0;
+//}
+//
+
+//int main()
+//{
+//	int a[10] = { 0 };
+//	int b = 0;
+//	for (b = 0; b<10; b++)
+//	{
+//		scanf("%d", &a[b]);
+//	}
+//	int 最大值 = a[0];
+//	for(b=1;b<10;b++)
+//	{ 
+//		if(a[b]>最大值)
+//		{
+//			最大值=a[b];
+//	    }
+//	}
+//	printf("最大值=%d\n", 最大值);
+//	return 0;
+//}
+//int main()
+//{
+//	int a[10] = {0};
+//	int b = 0;
+//    for (b = 0; b<10; b++)
+//	{   
+//		scanf("%d", &a[b]);
+//	}
+//	float c = 0;
+//	for (b = 0; b < 10; b++)
+//	{
+//		c = c + a[b];
+//	}
+//	float 平均值 = c / 10;
+//	printf("平均值 = %f\n", 平均值);
+//	return 0;
+//}
+/*nt main()
+{
+	int a[10] = { 0 };
+	int b = 0;
+	float c = 0;
+	for(b=0;b<10;b++)
+	{
+		scanf("%d", &a[b]);
+		c = c + a[b];
+	}
+	float 平均值 = c / 10;
+
+	return 0;*/
+
+	//int main()
+	//{
+	//	int a[10] = { 0 };
+	//	int b = 0;
+	//	for (b = 0;b < 10;b++)
+	//	{
+	//		scanf("%d", &a[b]);
+	//
+	//	}
+	//	int 最大值 = a[0];
+	//	for (b = 1;b < 10;b++)
+	//	{
+	//		if (a[b] > 最大值)
+	//		{
+	//			最大值 = a[b];
+	//		}
+	//	}
+	//	printf("%d", 最大值);
+	//	return 0;
+	//}
+//int main()
+//{
+//	int a[10] = { 0 };
+//
+//	int b = 0;
+//	float c = 0;
+//	for (b = 0;b < 10;b++)
+//	{
+//		scanf("%d", &a[b]);
+//		c = c + a[b];
+//	}
+//	float 平均值 = c / 10;
+//	printf("平均值=%f", 平均值);
+//	return 0;
+//}
+//int  main()
+//{
+//	printf( "%d",15-37);
+//		return 0;
+//} 
+//int main()
+//{
+//	//printf("喂！\n\n您好！\n再见。");
+//	int a = 0, b = 0, c = 0;
+//	scanf("%d", & c);
+//	scanf("%d", & a);
+//	scanf("%d", & b);
+//	printf("它们的和是:%d\n",a+b+c);
+//	return 0;
+//} 
+//int main()
+//{
+//	int a, b;
+//	scanf("%d", &a);
+//	scanf("%d", &b);
+//	printf("a的值是b的%d%%\n",a*100/b);
+//	return 0;
+//}
+//int main()
+//{
+//	int a=0;
+//	printf("我帅嘛？");
+//	scanf("%d", a);
+//	{
+//		printf("你错了", a = 0);
+//		printf("你对了", a = 1);
+//	}
+//	return 0;
+//}
+//int mian()
+//{
+//	int a[10] = { 0 };
+//	int b = 0;
+//	float c = 0;
+//	for (b = 0;b < 10;b++)
+//	{
+//		scanf("%d", &a[b]);
+//		c = c + a[b];
+//	}
+//	float 平均值 = c / 10;
+//	printf("平均值是=%f",平均值);
+//	return 0;
+//}
+//int main()
+//{
+//	printf("sdcsfa");
+//	return 0;
+//}
+/*nt main()
+{
+	int a[10] = { 0 };
+	int b = 0;
+	float c = 0;
+	printf("请输入十个整数：\n");
+	for (b = 0;b < 10;b++);
+	{
+		scanf("%d",&a[b]);
+		c = c + a[b];
+	}
+	float 平均值 = c / 10;
+	printf("平均值是=%f", 平均值);
+	return 0;
+}*/
+//int main()
+//{
+//	int a[10] = { 0 }, b;
+//	int c = 0;
+//	printf("请输入十个整数;\n");
+//	for (b = 0;b < 10;b++)
+//	{
+//		scanf("%d",&a[b]);
+//		c = c + a[b];
+//	}
+//	float 平均值 = c / 10;
+//	printf("平均值:%f\n", 平均值);
+//	return 0;
+//} 
+//int main()
+//{
+//	int a = 0;
+//	printf("你认为我帅嘛（0（是的）/1（不是））？\n");
+//	scanf("%d", &a);
+//	if (a == 0)
+//	printf("嘿嘿，我就知道我最帅\n");
+//	else
+//		printf("哼！我不管，我最帅！\n");
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 0;
+//	printf("您的身高是:"); scanf("%d", &a);
+//	printf("您的标准体重是:%3.1f斤\n",(a-100)*1.8);
+//	return 0;
+//}
+//
+//int main(void)
+//{
+//	int num;
+//	printf("请输入一个整数；");
+//	scanf("%d",&num);
+//	if(num-5)
+//		puts("哈哈哈");
+//	else
+//		puts("该整数");
+//	return 0;
+//}
+//int main()
+//{
+//	int a,b,c,最小值;
+//	scanf("%d%d%d", &a,&b,&c);
+//	最小值 = (a < b) ? a : b;
+//	最小值 = (最小值 < c) ? 最小值 : c;
+//	printf("最小值是:%d\n", 最小值);
+//	return 0;
+//}
+//int main()
+//{
+// 
+//	int a, b, c;
+
+//	printf("请输入三个整数：\n");
+//	printf("整数A:");  scanf("%d",&a);
+//	printf("整数B:");  scanf("%d",&b);
+//	printf("整数C:");  scanf("%d",&c);
+//	if (b == a && a == c)printf("三个数相等\n");
+//	else if (a==b||b==c||a==c)printf("有两个数相等\n");
+//	else printf("三个数都不相等\n");
+//	return 0;
+//}
+//int main()
+//{
+//	int a, b;
+//	printf("请输入俩个整数：\n");
+//	printf("整数A:");  scanf("%d", &a);
+//	printf("整数B:");  scanf("%d", &b);
+//	if (a - b >= 0 && a - b > 10) printf("它们的差值大于等于11\n");
+//	else if (b - a >= 0 && b - a < 10) printf("它们的差值小于等于10\n");
+//	return 0;
+//}
+//int main()
+//{
+//	int a, b,c,e;
+//	printf("请输入两个整数：\n");
+//	printf("整数1：");scanf("%d", &a);
+//	printf("整数2：");scanf("%d", &b);
+//	c = (a - b < 0) ? a : b;
+//	e = c;
+//	do {
+//		c = c + 1;
+//		e = e + c + 1;
+//	} while (c <= b);
+//	if (a > b)
+//		printf("%d与%d之间的整数和为%d\n", b, a, e);
+//	else if (a == b)
+//		printf("因为这两个数相等，所以它们之间的整数和为0");
+//	else
+//		printf("%d与%d之间的整数和为%d\n", a,b, e);
+//
+//	/*if (a > b)
+//	{
+//		do {
+//			c = b + 1;
+//			d = b;
+//			d = d + b + 1;
+//		} while (c > a);
+//		printf("%d与%d之间的整数和为%d\n", b, a, d);
+//	}
+//	else if (a == b)
+//	{
+//		printf("因为这两个数相等，所以它们之间的整数和为0");
+//	}
+//	else
+//	{
+//	    do {
+//			c = a + 1;
+//			d = a;
+//			d = d + a + 1;
+//		} while (c <= b);
+//		printf("%d与%d之间的整数和为%d\n", a, b, d);
+//	}*/
+//	return 0;
+//}
+//int main()
+//{
+//	int a, b;
+//	scanf("%d", &a);scanf("%d", &b);
+//	if (a > b)
+//	{
+//		do {
+//			b++;
+//		} while (b <a);
+//		printf("B:%d,A:%d", a, b);
+//	}
+//	else
+//	{
+//		do {
+//			a++;
+//		} while (b >a);
+//		printf("A:%d,B:%d",a,b);
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int a, b;
+//	printf("请输入两个整数\n");
+//	printf("整数1：");scanf("%d", &a);
+//	printf("整数2：");scanf("%d", &b);
+//	if (a > b)
+//	{
+//		int c = b;
+//		int d = b;
+//		do
+//		{
+//			d++;
+//			c = c + d;
+//		} while (a > d);
+//		printf("%d与%d之间的整数和为%d", b, a, c);
+//	}
+//	else if (a == b)
+//		printf("因为这两个数相等，所以它们之间的整数和为0");
+//	else
+//	{
+//		int d = a;
+//		int c = a;
+//		do
+//		{
+//			c++;
+//			d = d + c;
+//		} while (b > c);
+//		printf("%d与%d之间的整数和为%d", a, b, d);
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int a;
+//	printf("请输入一个整数：");
+//	scanf("%d", &a);
+//	while (a >=0)
+//	{
+//		if (a > 0)
+//		{
+//			printf("%d ", a);
+//			a--;
+//			printf("\n");
+//		}
+//		else if (a == 0)
+//		{
+//			printf("0\a");a--;
+//		}
+//		else;
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int a;
+//	printf("请输入一个整数：");
+//	scanf("%d", &a);
+//	while (a >=1)
+//	{
+//		if (a >1)
+//		{
+//			printf("%d ", a--);
+//		}
+//		else if (a == 1)
+//		{
+//			printf("%d",a--);
+//		}
+//		else;
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int a,b;
+//	printf("正整数：");scanf("%d", &a);b = 1;
+//	while (a >= b)
+//	{
+//		b++;
+//		if (b % 2)
+//			printf("-");
+//		else
+//			printf("+");
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int a,b;
+//	printf("正整数：");scanf("%d", &a);b = 0;
+//	while (b < a)
+//	{
+//		b++;
+//		printf("*");printf("\n");
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int a, b,d;
+//	printf("要输入多少个整数：");scanf("%d", &a);b = 0;d = 0;
+//	while (a > b)
+//	{
+//		int c;
+//		printf("No%d:", ++b);scanf("%d", &c);
+//		d = d + c;
+//	}
+//	printf("和：%d,平均值：%.2f", d, (double)d / b);
+//	return 0;
+//}
+//int main()
+//{
+//	int a, b;
+//	printf("请输入一个正整数：");scanf("%d", &a);
+//	if (a < 0)
+//		printf("请不要输入负数");
+//	while (a > 0)
+//	{
+//		printf("%d", a % 10);
+//		a /= 10;
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int a, b;
+//	printf("请输入一个正整数：");scanf("%d", &a);b = 0;
+//	while (a % 10)
+//	{
+//		b++;a /= 10;
+//	}
+//	printf("这个整数是%d位数。", b);
+//	return 0;
+//}
+//
+//int main()
+//{
+//	int a, b;
+//	printf("n的值：");scanf("%d", &a);
+//	for (b = 1;b <= a;b++)
+//	{
+//		printf("%d的平方是%d。\n", b, b * b);
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int a, b,c;
+//	printf("请输入一个整数：");scanf("%d", &a);c = 1;
+//	for (b = 0;b < a;b++)
+//	{
+//		if (c <10)
+//		{
+//			printf("%d", c++);
+//		}
+//		if(c==10)
+//		{
+//			c = 0;
+//		}
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int a, b,c;
+//	printf("要显示多少个*:");scanf("%d", &a);c = 0;
+//	for (b = 0;b < a;b++)
+//	{
+//		if (c < 5)
+//		{
+//			printf("*", c++);
+//		}
+//		if (c == 5)
+//		{
+//			c = 0;printf("\n");
+//		}
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int b, c,d;
+//	printf("请输入一个整数:");scanf("%d", &d);c = 0;
+//	for (b = 1;b <=d;b++)
+//	{
+//		if (d % b == 0)
+//		{
+//			printf("%d\n", b);
+//			c++;
+//		}
+//		else;
+//	}
+//	printf("约数有%d个", c);
+//	return 0;
+//}
+//int main()
+//{
+//	int a, b, c;
+//	printf("请输入一个整数：");scanf("%d",&a)
+//
+//	return 0;
+//}
+//int main()
+//{
+//	int a, b;
+//	for (a = 1;a <= 9;a++)
+//	{
+//		for (b = 1;b <= 9;b++)
+//		{
+//			printf("%3d", a * b);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int a, b,c,d;
+//	printf("让我们来画个长方形\n");printf("一边长为:");scanf("%d", &a);
+//	printf("另一边为:");scanf("%d", &b);
+//	if (a < b)
+//	{
+//		for (c = 1;c <= a;c++)
+//		{
+//			for (d = 1;d <= b;d++)
+//			{
+//				printf("*");
+//			}
+//			printf("\n");
+//		}
+//	}
+//	else
+//	{
+//		for (c = 1;c <= b;c++)
+//		{
+//			for (d = 1;d <= a;d++)
+//			{
+//				printf("*");
+//			}
+//			printf("\n");
+//		}
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int a, b, c;
+//	printf("生成直角在左上角的直角三角形\n");
+//	printf("短边为:");scanf("%d", &a);
+//	for (b = a;b>0;b--)
+//	{
+//		for (c =b;c > 0;c--)
+//		{
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int a, b, c, d;
+//	printf("生成一个直角在右上方的直角三角形\n");
+//	printf("短边:");scanf("%d", &a);
+//	for (b=a;b>0;b--)
+//	{
+//		for (c = 1;c <= a - b;c++)
+//			printf(" ");
+//		for (d= b;d > 0;d--)
+//			printf("*");
+//		printf("\n");
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int a, b, c,d,e;
+//	printf("让我们来画个金字塔.\n");
+//	printf("有几层:");scanf("%d", &a);e = 1;
+//	for (b = a;b > 0;b--)
+//	{
+//		for (c = b;c > 0;c--)
+//		{
+//			printf(" ");
+//		}
+//		for (d = 0;d <(2*e-1);d++)
+//		{
+//			printf("*");
+//		}
+//		printf("\n");e++;
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int a, b, c,d;
+//	printf("让我们来画个向下的金字塔。\n");
+//	printf("有几层:");scanf("%d", &a);
+//	for (b = 1;b <= a;b++)
+//	{
+//		for (c = 1;c < b;c++)
+//		{
+//			printf(" ");
+//		}
+//		for (d =2*(a-b)+1;d>0;d--)
+//		{
+//			printf("%d", c % 10);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	printf("12233333""88888");
+//	return 0;
+//}
